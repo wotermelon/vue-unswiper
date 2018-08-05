5bdabaeb42358124fd9e5d2c24f03036893f8a7b
张铭泽 <zhangmingze@xunlei.com>
2018-08-06 00:39:07 +0800

    只保留最多三张，动态创建销毁

--- src/unswiper/core.js
+++ src/unswiper/core.js
@@ -1,105 +1,137 @@
 const DEFAULT_CONFIG = {
   container: null,
   duration: 300,
-  threshold: 150
+  threshold: 100
 }
 
 class Unswiper {
-  constructor (options) { 
+  constructor (options) {
     this.options = Object.assign({}, DEFAULT_CONFIG, options)
 
     this.containerEl = this.options.container
-    console.log(this.containerEl)
-    this.width = this.containerEl.offsetWidth
+
+    this.width = document.body.clientWidth
     this.heigh = this.containerEl.offsetHeight
 
     this.pos = {
       startX: 0,
-      startY: 0,
       moveX: 0,
-      moveY: 0,
-      endX: 0,
-      endY: 0
     }
 
     this.offset = 0
     this.activeIndex = 0
     this.needSlide = false
+    this.direction = '' // next, prev
+    this.isAnimating = false
+
+    this.slideEvents = {
+      slideStart: [],
+      slideEnd: [],
+      slideNextEnd: [],
+      slidePrevEnd: []
+    }
+
+    this.dm = {
+      prev: 'slidePrevEnd',
+      next: 'slideNextEnd'
+    }
+    let self = this
+    this.onTouchStartBind = this.onTouchStart.bind(self)
+    this.onTouchMoveBind = this.onTouchMove.bind(self)
+    this.onTouchEndBind = this.onTouchEnd.bind(self)
+
 
     this.init()
     this.bindEvent()
-  } 
+  }
   /**
    * 初始化
    */
   init () {
-    
+
   }
   /**
    * 绑定手势
    */
-  bindEvent () { 
-    this.containerEl.addEventListener('touchstart', this.onTouchStart.bind(this), false)
-    this.containerEl.addEventListener('touchmove', this.onTouchMove.bind(this), false)
-    this.containerEl.addEventListener('touchend', this.onTouchEnd.bind(this), false)
+  bindEvent () {
+    this.containerEl.addEventListener('touchstart', this.onTouchStartBind, false)
 
-    this.containerEl.addEventListener('transitionend', () => { 
-      console.log('end')
+    this.containerEl.addEventListener('transitionend', () => {
+      console.log('transitionend')
+      // this.isAnimating = false
+      setTimeout(() => {
+        this.isAnimating = false
+      }, 50);
+
+      if (this.needSlide) {
+        this.emit(this.dm[this.direction])
+      }
     }, false)
   }
   onTouchStart (e) {
-    console.log(e)
-    this.pos.startX = e.changedTouches[0].pageX
-    this.pos.startY = e.changedTouches[0].pageY
-    
-    this.containerEl.classList.add('no-transition')
+    if (!this.isAnimating) {
+      this.containerEl.addEventListener('touchmove', this.onTouchMoveBind, false)
+      this.containerEl.addEventListener('touchend', this.onTouchEndBind, false)
+
+      this.pos.startX = e.touches[0].pageX
+      this.pos.startY = e.touches[0].pageY
+
+      this.containerEl.classList.add('no-transition')
+    }
   }
   onTouchMove (e) {
-    this.pos.moveX = e.changedTouches[0].pageX
-    this.pos.moveY = e.changedTouches[0].pageY
+    if (!this.isAnimating) {
+      this.pos.moveX = e.touches[0].pageX
 
-    let distance = (this.pos.moveX - this.pos.startX) / 2
+      let distance = (this.pos.moveX - this.pos.startX)
 
-    this.setStyle(this.containerEl, 'transform', `translate3d(${+ (distance - this.offset)}px, 0, 0)`)
-    
-    e.preventDefault()
+      this.setStyle(this.containerEl, 'transform', `translate3d(${+ (distance - this.offset)}px, 0, 0)`)
+    }
   }
   onTouchEnd (e) {
-    this.pos.endX = e.changedTouches[0].pageX
-    this.pos.endy = e.changedTouches[0].pageY
+    let distance = this.pos.moveX - this.pos.startX
+    this.destroy()
 
-    let distance = this.pos.endX - this.pos.startX
+    if (distance && !this.isAnimating) {
+      // 向右滑动，上一张
+      if (distance > this.options.threshold) {
+        if (this.activeIndex > 0) {
+          this.activeIndex = this.activeIndex - 1
+          this.needSlide = true
+        } else {
+          this.needSlide = false
+        }
+        this.direction = 'prev'
+      } else if (-distance > this.options.threshold) {
+        // 向左滑动，下一张
+        let num = this.containerEl.children.length
 
-    // 向右滑动，上一张
-    if (distance > this.options.threshold) { 
-      this.activeIndex = this.activeIndex === 0 ? 0 : --this.activeIndex
+        if (this.activeIndex < num - 1) {
+          this.activeIndex = this.activeIndex + 1
+          this.needSlide = true
 
-      console.log('上', this.activeIndex)
-      console.log(distance, this.options.threshold)
-      this.needSlide = true
-    } else if (-distance > this.options.threshold) {
-      // 向左滑动，下一张
-      let num = this.containerEl.children.length
-      this.activeIndex = this.activeIndex < num - 1 ? ++this.activeIndex : this.activeIndex
+        } else {
+          this.needSlide = false
+        }
 
-      console.log('下', this.activeIndex)
-      console.log(-distance, this.options.threshold)
+        this.direction = 'next'
+      } else {
+        this.needSlide = false
+      }
 
-      this.needSlide = true
+      this.slideTo(this.activeIndex)
     }
-
-    this.slideTo(this.activeIndex)
-
-    e.preventDefault()
   }
   slideTo (index, animate = true) {
     this.offset = index * this.width
+    this.activeIndex = index
 
     if (animate) {
+      this.isAnimating = true
       this.containerEl.classList.remove('no-transition')
       this.setStyle(this.containerEl, 'transition-duration', this.options.duration + 'ms')
 
-    } else { 
+    } else {
       this.containerEl.classList.add('no-transition')
     }
 
@@ -109,13 +141,23 @@ class Unswiper {
     el.style[`-webkit-${key}`] = style
     el.style[key] = style
   }
-  getWidth () { 
+  getWidth () {
     return this.width
   }
   destroy () {
-    this.containerEl.removeEventListener('touchstart')
-    this.containerEl.removeEventListener('touchmove')
-    this.containerEl.removeEventListener('touchend')
+    // this.containerEl.removeEventListener('touchstart')
+    this.containerEl.removeEventListener('touchmove', this.onTouchMoveBind)
+    this.containerEl.removeEventListener('touchend', this.onTouchEndBind)
+  }
+  on (eventName, cb) {
+    if (!this.slideEvents[eventName]) {
+      return
+    }
+
+    this.slideEvents[eventName].push(cb)
+  }
+  emit (eventName) {
+    this.slideEvents[eventName].forEach(fn => fn())
   }
 }
 
--- src/unswiper/index.vue
+++ src/unswiper/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="unswiper-container">
-    <div class="wrapper" :id="wrapperId"> 
-      <div 
+  <div class="unswiper-container" @touchmove="fn">
+    <div class="wrapper" :id="wrapperId">
+      <div
         v-for="item in unswiperList"
-        :key="item"
+        :key="item.id"
         class="slider"
         :style="{ width: clientWidth + 'px' }">
         <slot :data="item"></slot>
@@ -26,7 +26,8 @@ export default {
     return {
       clientWidth: '',
       wrapperId: '',
-      unswiperList: [1, 2, 3, 4, 5, 6, 7, 8]
+      listIndex: 0,
+      unswiperList: []
     }
   },
   created () {
@@ -36,16 +37,83 @@ export default {
     this.unswiper = new Unswiper({
       container: document.querySelector('#' + this.wrapperId)
     })
+    this.unswiper.on('slideEnd', () => {
+      this.$emit('slideend')
+    })
+    this.unswiper.on('slideNextEnd', () => {
+      this.$emit('slidenextend')
+
+      if (this.listIndex === 0) {
+        this.unswiperList.push(this.data[2])
+
+      } else if (this.listIndex < this.data.length - 2) {
+        this.unswiper.slideTo(1, false)
+
+        this.unswiperList.push(this.data[this.listIndex + 2])
+        this.unswiperList.shift()
+      } else if (this.listIndex === this.data.length - 2) {
+        this.unswiper.slideTo(1, false)
+        this.unswiperList.shift()
+      }
+
+      this.listIndex = this.listIndex + 1
+    })
+
+    this.unswiper.on('slidePrevEnd', () => {
+      this.$emit('slideprevend')
+
+      if (this.listIndex === this.data.length - 1) {
+        this.unswiper.slideTo(1, false)
+        this.unswiperList.unshift(this.data[this.data.length - 3])
+
+      } else if (this.listIndex >= 2) {
+        this.unswiper.slideTo(1, false)
+
+        this.unswiperList.unshift(this.data[this.listIndex - 2])
+        this.unswiperList.pop()
+      } else if (this.listIndex === 1) {
+        this.unswiperList.pop()
+      }
+
+      this.listIndex = this.listIndex - 1
+    })
     this.clientWidth = this.unswiper.getWidth()
+  },
+  methods: {
+    fn (e) {
+      e.preventDefault()
+    },
+    init (index) {
+      this.listIndex = index
+      let innerIndex = 1
+
+      if (index === 0) {
+        this.unswiperList = [
+          this.data[index],
+          this.data[index + 1]
+        ]
+        innerIndex = 0
+      } else if (index === this.data.length - 1) {
+        this.unswiperList = [
+          this.data[index - 1],
+          this.data[index]
+        ]
+      } else {
+        this.unswiperList = [
+          this.data[index - 1],
+          this.data[index],
+          this.data[index + 1]
+        ]
+      }
+      this.$nextTick(() => {
+        this.unswiper.slideTo(innerIndex, false)
+      })
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  * {
-    box-sizing: border-box;
-  }
-
   .unswiper-container {
     width: 100%;
     height: 100%;
@@ -60,6 +128,7 @@ export default {
 
   .wrapper {
     position: relative;
+    z-index: 10;
     width: 100%;
     display: flex;
     flex: 1;
